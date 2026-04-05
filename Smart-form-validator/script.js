80cc6312086db918590a56fb7a11a0b7205e46f9
Sushma <saraganacharisushma@gmail.com>
2026-04-05 16:39:28 +0530

    Added smart form validator

---
+++ Smart-form-validator/script.js
@@ -0,0 +1,39 @@
+const form = document.getElementById("form");
+const nameInput = document.getElementById("name");
+const emailInput = document.getElementById("email");
+const passwordInput = document.getElementById("password");
+const errors = document.querySelectorAll(".error");
+
+form.addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  let isValid = true;
+
+  // Name validation
+  if (nameInput.value.trim() === "") {
+    errors[0].innerText = "Name is required";
+    isValid = false;
+  } else {
+    errors[0].innerText = "";
+  }
+
+  // Email validation
+  if (!emailInput.value.includes("@")) {
+    errors[1].innerText = "Enter valid email";
+    isValid = false;
+  } else {
+    errors[1].innerText = "";
+  }
+
+  // Password validation
+  if (passwordInput.value.length < 6) {
+    errors[2].innerText = "Min 6 characters required";
+    isValid = false;
+  } else {
+    errors[2].innerText = "";
+  }
+
+  if (isValid) {
+    document.getElementById("successMsg").innerText = "Form Submitted Successfully!";
+form.reset();  }
+});
