68786c6601012b19b1b833d953f77be283a3f6ab
Sushmacs1712 <saraganacharisushma@gmail.com>
2026-07-12 23:44:13 +0530

    Add Enter key support for adding topics

--- Interview-Prep-Tracker/script.js
+++ Interview-Prep-Tracker/script.js
@@ -66,6 +66,19 @@ function updateProgress() {
   progressText.textContent = `Progress: ${progress}%`;
   progressFill.style.width = `${progress}%`;
 }
+function toggleDarkMode() {
+  document.body.classList.toggle("dark");
+
+  if (document.body.classList.contains("dark")) {
+    localStorage.setItem("theme", "dark");
+  } else {
+    localStorage.setItem("theme", "light");
+  }
+}
+
+if (localStorage.getItem("theme") === "dark") {
+  document.body.classList.add("dark");
+}
 
 function displayTopics() {
   const topicList = document.getElementById("topicList");
@@ -108,3 +121,9 @@ if (currentFilter === "pending") {
 }
 
 displayTopics();
+
+document.getElementById("topicInput").addEventListener("keypress", function (e) {
+  if (e.key === "Enter") {
+    addTopic();
+  }
+});
